4592e223fa70f074c24db3e11cc1b60f287da6ee
[Daniel Sun] <dansun@stanford.edu>
2026-01-05 14:26:33 -0500

    basic python

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Hello World for PSI Numerical Methods 2026
 # Hello World from WSL
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 def myexp(x, N=10):
     """
     This function computes exp(x) via the Taylor Series using terms up to
@@ -24,7 +27,16 @@ if __name__ == "__main__":
 
     print("Hello, world!")
 
-    print("e(1) with  5 terms is", myexp(1.0, 5))
-    print("e(1) with 10 terms is", myexp(1.0, 10))
-    print("e(1) with 20 terms is", myexp(1.0, 20))
-    print("e(1) with 40 terms is", myexp(1.0, 40))
+    print("e^(1.2)={}".format(np.exp(1.2)))
+
+    x = np.linspace(0, 2 * np.pi, 1000)
+    fig, ax = plt.subplots(figsize = (8, 6), dpi = 300, layout = "constrained")
+    plt.plot(x, np.sin(x), label = r"$\sin(x)$")
+    plt.plot(x, np.cos(x), label = r"$\cos(x)$")
+    plt.legend()
+    plt.savefig("trig.png")
+
+    # print("e(1) with  5 terms is", myexp(1.0, 5))
+    # print("e(1) with 10 terms is", myexp(1.0, 10))
+    # print("e(1) with 20 terms is", myexp(1.0, 20))
+    # print("e(1) with 40 terms is", myexp(1.0, 40))
